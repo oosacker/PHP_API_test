bcf46fb949bdc768b75b46f0ca5220626ccf4f43
Natsuki Hasegawa <oosacker@gmail.com>
2020-09-23 21:50:01 +1200

    required input

--- guzzletest.php
+++ guzzletest.php
@@ -6,23 +6,38 @@ use GuzzleHttp\Client;
 require 'vendor/autoload.php';
 
 $client = new Client([
-    'base_uri' => 'https://reqres.in',
+    //'base_uri' => 'https://reqres.in',
+    'base_uri' => 'http://45.76.116.27/ARESAPI',
     'timeout'  => 2.0,
 ]);
 
-try {
-    $r = $client->request('POST', '/api/users', [
-        'json' => [
-            'Content-Type' => 'application/json',
-            'Accept' => 'application/json'
-        ],
-        'form_params' => [
-            'name' => 'my_name',
-            'job' => 'my_job'
-        ]
-    ]);
-    echo $r->getBody();
+function doPost($url, $body, $client)
+{
+
+
+    try {
+
+        //$url = '/products/changed'; // '/api/users/'
+
+        $r = $client->request('POST', $url, [
+            'json' => [
+                'Content-Type' => 'application/json',
+                'Accept' => 'application/json'
+            ],
+            'form_params' => $body
+
+//            'form_params' => [
+//                //'name' => 'my_name',
+//                //'job' => 'my_job'
+//                'since' => '2010-09-20T01:50:24.181Z'
+//            ]
+        ]);
+        echo $r->getBody();
+    }
+    catch (\GuzzleHttp\Exception\GuzzleException $e) {
+        echo $e->getMessage();
+        echo $e->getTrace();
+    }
 }
-catch (\GuzzleHttp\Exception\GuzzleException $e) {
-    echo $e->getMessage();
-}
+
+doPost('/products/changed', ['since' => '2019-09-20T01:50:24.181Z'], $client);
